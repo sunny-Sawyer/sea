f258d0364e569c9d41107050d42a6ecaebb95ae1
sunny-Sawyer <2603001448@qq.com>
2023-09-04 21:22:22 +0800

    first commit

---
+++ test_9_4/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int age = 20;
+	double weight = 75.3;
+	age = age + 1;
+	weight = weight - 10;
+	printf("%d\n", age);
+	printf("%lf\n", weight);
+
+	return 0;
+}
