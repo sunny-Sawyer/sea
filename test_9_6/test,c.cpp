7f95fe4ed391ff5d873fc02153490050ae53f135
sunny-Sawyer <2603001448@qq.com>
2023-09-06 15:59:28 +0800

    first commit

---
+++ test_9_6/test,c.cpp
@@ -0,0 +1,15 @@
+enum Sex
+{
+	MALE,
+	FEMALE,
+	SECRET
+};
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", MALE);
+	printf("%d\n", FEMALE);
+	printf("%d\n", SECRET);
+
+	return 0;
+}
---
+++ test_9_6/test.c
@@ -0,0 +1,15 @@
+enum Sex
+{
+	MALE = 3,
+	FEMALE,
+	SECRET
+};
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", MALE);
+	printf("%d\n", FEMALE);
+	printf("%d\n", SECRET);
+
+	return 0;
+}
