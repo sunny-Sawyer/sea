c87da01a68b719180195ed969ce7a80146f6ff1c
sunny-Sawyer <2603001448@qq.com>
2023-10-18 21:31:47 +0800

    first commit

---
+++ test_10_18/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	char a, b, c;
+	putchar(65);
+	a = getchar();
+	b = getchar();
+	c = getchar();
+	putchar(a);
+	printf("\n");
+	putchar(b);
+	printf("\n");
+	putchar(c);
+
+
+	return 0;
+}
